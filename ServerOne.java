8ab8515cc89679e9dcaac1ab9ab4f7e21040d648
Saianeesh Vinmani <imsaianeesh17@gmail.com>
2024-02-24 20:28:05 -0700

    init commit

---
+++ ServerOne.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.net.*;
+
+public class ServerOne{
+    public ServerOne(){
+
+    }
+
+    Socket socket;
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
