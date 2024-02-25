5098a370753f72a6348b1bb005fdcc6c9c169c33
Saianeesh Vinmani <imsaianeesh17@gmail.com>
2024-02-24 20:30:01 -0700

    added sockets

--- ServerOne.java
+++ ServerOne.java
@@ -2,11 +2,15 @@ import java.io.*;
 import java.net.*;
 
 public class ServerOne{
+    
+    Socket socket;
+    ServerSocket serverSocket;
+    
     public ServerOne(){
 
     }
 
-    Socket socket;
+    
     public static void main(String[] args){
         System.out.println("Hello World");
     }
