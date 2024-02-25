9341c37fca701cd5c5b9712134880e1779400c3f
Saianeesh Vinmani <imsaianeesh17@gmail.com>
2024-02-24 20:44:42 -0700

    made server

--- ServerOne.java
+++ ServerOne.java
@@ -2,16 +2,41 @@ import java.io.*;
 import java.net.*;
 
 public class ServerOne{
-    
-    Socket socket;
+
+    int port = 2025;
+    Socket server;
     ServerSocket serverSocket;
+    PrintWriter out;
+    BufferedReader in;
     
     public ServerOne(){
-
+        
+        
     }
 
+    public void startServer() {
+        try {
+            serverSocket = new ServerSocket(this.port);
+            System.out.println("Server listening on port" + port);
+            server = serverSocket.accept();
+            out = new PrintWriter(server.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(server.getInputStream()));
+            String greeting = in.readLine();
+            if ("hello server".equals(greeting)) {
+                out.println("hello client");
+            }
+            else {
+                out.println("unrecognised greeting");
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
     
     public static void main(String[] args){
-        System.out.println("Hello World");
+        // System.out.println("Hello World");
+        ServerOne serverOne =  new ServerOne();
+        serverOne.startServer();
     }
 }
