d3812cfc26b808791dff186764de70179e71d4eb
dojukwu10 <117415397+dojukwu10@users.noreply.github.com>
2024-03-02 22:20:37 -0700

    Create ServerThree.java

---
+++ ServerThree.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.net.*;
+
+public class ServerThree{
+
+    int port = 2027;
+    Socket server;
+    ServerSocket serverSocket;
+    PrintWriter out;
+    BufferedReader in;
+    
+    public ServerThree(){
+        
+        
+    }
+
+    public void startServer() {
+        try {
+            serverSocket = new ServerSocket(this.port);
+            System.out.println("Server listening on port" + port);
+            server = serverSocket.accept();
+            out = new PrintWriter(server.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(server.getInputStream()));
+            String greeting = in.readLine();
+            if ("hello server".equals(greeting)) {
+                out.println("Server1");
+            }
+            else {
+                out.println("unrecognised greeting");
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args){
+        // System.out.println("Hello World");
+        ServerThree serverThree =  new ServerThree();
+        serverThree.startServer();
+    }
+}
+ort 
