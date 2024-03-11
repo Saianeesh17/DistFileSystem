06e1734e49f676d90d2365a2b65493bacf64364f
Saianeesh Vinmani <imsaianeesh17@gmail.com>
2024-03-11 17:12:56 -0600

    modified client to add functionality to send file streams

--- Client.java
+++ Client.java
@@ -1,38 +1,52 @@
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
     
-    private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
+    private static Socket clientSocket;
+    private static DataOutputStream out;
+    private static DataInputStream in;
     private String resp;
 
     public void startConnection(String ip, int port) {
         
         try{
             clientSocket = new Socket(ip, port);
-            out = new PrintWriter(clientSocket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out = new DataOutputStream(clientSocket.getOutputStream());
+            in = new DataInputStream(clientSocket.getInputStream());
         } catch (Exception e){
             e.printStackTrace();
         }
     }
 
-    public String sendMessage(String msg) {
-        try{
-            out.println(msg);
-            resp = in.readLine();
-        } catch (Exception e) {
-            e.printStackTrace();
+    public static void sendFile(String filepath) throws Exception {
+        File file = new File(filepath);
+        FileInputStream fileInputStream = new FileInputStream(file);
+        byte[] buffer = new byte[4 * 1024];
+        long fileSize = file.length();
+    
+        try {
+            // Send file size
+            out.writeLong(fileSize);
+    
+            // Send file content
+            int bytesRead;
+            while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+            }
+        } finally {
+            fileInputStream.close();
         }
-        return resp;
-        
     }
+    
 
-    public void stopConnection() {
+    public static void stopConnection() {
 
         try{
             out.close();
@@ -40,13 +54,20 @@ public class Client {
         } catch (Exception e){
             e.printStackTrace();
         }
+        System.out.println("connection terminated");
     }
 
     public static void main(String[] args){
         String serverAddress = "127.0.0.1";
         Client client = new Client();
-        client.startConnection(serverAddress, 2027);
-        String response = client.sendMessage("client3");
-        System.out.println(response);
+        client.startConnection(serverAddress, 2029);
+        try {
+            sendFile("large.jpg");
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        stopConnection();
+        // System.out.println(response);
     }
 }
