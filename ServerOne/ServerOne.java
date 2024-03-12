57ce46c637120adfc4d0eb57be2814c7e7f6770d
Ludovic Berset <ludovic.berset@hes-so.ch>
2024-03-11 18:23:15 -0600

    Status method added

--- ServerOne/ServerOne.java
+++ ServerOne/ServerOne.java
@@ -1,6 +1,9 @@
-// package ServerOne;
+package ServerOne;
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ServerOne{
 
@@ -9,6 +12,9 @@ public class ServerOne{
     ServerSocket serverSocket;
     PrintWriter out;
     BufferedReader in;
+    static String saveDirectory = "./received_files/";
+    private static DataOutputStream dos = null;
+    private static DataInputStream dis = null;
     
     public ServerOne(){
         
@@ -21,22 +27,72 @@ public class ServerOne{
             System.out.println("Server listening on port" + port);
             while(true){
                 server = serverSocket.accept();
-                out = new PrintWriter(server.getOutputStream(), true);
-                in = new BufferedReader(new InputStreamReader(server.getInputStream()));
-                String greeting = in.readLine();
-                if ("hello server".equals(greeting)) {
-                    out.println("Server1");
+                dis = new DataInputStream(server.getInputStream());
+                dos = new DataOutputStream(server.getOutputStream());
+                
+                String request = dis.readUTF();
+
+                switch(request){
+                    case "UPLOAD":
+                        receiveFile(saveDirectory + dis.readUTF());
+                    break;
+                    case "STATUS":
+                        statusCheck(server);
+                    break;
+                    default:
+                    System.out.println("Wrong request!\n");
+
                 }
-                else {
-                    out.println("unrecognised greeting");
-                }
+                
+                dis.close();
+                dos.close();
             }
         }
         catch(Exception e) {
             e.printStackTrace();
         }
     }
+
+    private static void statusCheck(Socket clientSocket) throws IOException{
+        // Get files name and put them into an array
+        String[] filesArray = getFilesArray();
+
+        //Sending the array back
+        dos.writeInt(filesArray.length);
+        for (String name : filesArray) {
+            dos.writeUTF(name);
+        }
+
+    }
+
+    private static String[] getFilesArray() {
+        File folder = new File(saveDirectory);
+        File[] listOfFiles = folder.listFiles();
+        String[] fileArray = new String[0];
+
+        if (listOfFiles != null) {
+            fileArray = Arrays.stream(listOfFiles)
+                    .filter(File::isFile)
+                    .map(File::getName)
+                    .toArray(String[]::new);
+        }
+
+        return fileArray;
+    }
     
+    private static void receiveFile(String filePath) throws Exception{
+        FileOutputStream fos = new FileOutputStream(filePath);
+        long fileSize = dis.readLong();
+        byte[] buffer = new byte[4096];
+
+        int bytesRead;
+        while (fileSize > 0 && (bytesRead = dis.read(buffer, 0, (int)Math.min(buffer.length, fileSize))) != -1) {
+            fos.write(buffer, 0, bytesRead);
+            fileSize -= bytesRead;
+        }
+        fos.close();
+    }
+
     public static void main(String[] args){
         // System.out.println("Hello World");
         ServerOne serverOne =  new ServerOne();
