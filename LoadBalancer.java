5857817a1458bc6bf1e2ccfa6630417a94340004
Saianeesh Vinmani <imsaianeesh17@gmail.com>
2024-03-13 16:10:26 -0600

    Revert "converted filecontents array to arrayList"
    This reverts commit 271381c15640bb3f3c3a7527e0d1028c604e19e8.

--- LoadBalancer.java
+++ LoadBalancer.java
@@ -87,9 +87,7 @@ public class LoadBalancer {
 
         public static int leader = 0;
 
-        // static String[][] fileContents = new String[3][];
-
-        static ArrayList<String[]> fileContents = new ArrayList<>();
+        static String[][] fileContents = new String[3][];
 
         @Override
         public void run() {
@@ -124,7 +122,7 @@ public class LoadBalancer {
                         documentNames[j] = in.readUTF();
                     }
 
-                    fileContents.set(i, Arrays.copyOf(documentNames, documentNames.length));
+                    fileContents[i] = Arrays.copyOf(documentNames, documentNames.length);
                         
                     // Print the received array
                     System.out.println("Documents on server:");
@@ -141,11 +139,11 @@ public class LoadBalancer {
             }
 
             HashMap<Integer, String[][]> differences = new HashMap<>();
-            if (isRunning[(leader + 1) % SERVER_PORTS.size()] && !fileContents.get((leader + 1) % SERVER_PORTS.size()).equals(fileContents.get(leader))) {
-                differences.put((leader + 1) % SERVER_PORTS.size(), compareArrays(fileContents.get(leader), fileContents.get((leader + 1) % SERVER_PORTS.size())));
+            if (!fileContents[(leader + 1) % SERVER_PORTS.size()].equals(fileContents[leader])) {
+                differences.put((leader + 1) % SERVER_PORTS.size(), compareArrays(fileContents[leader], fileContents[(leader + 1) % SERVER_PORTS.size()]));
             }
-            if (isRunning[(leader + 2) % SERVER_PORTS.size()] && !fileContents.get((leader + 2) % SERVER_PORTS.size()).equals(fileContents.get(leader))) {
-                differences.put((leader + 2) % SERVER_PORTS.size(), compareArrays(fileContents.get(leader), fileContents.get((leader + 2) % SERVER_PORTS.size())));
+            if (!fileContents[(leader + 2) % SERVER_PORTS.size()].equals(fileContents[leader])) {
+                differences.put((leader + 2) % SERVER_PORTS.size(), compareArrays(fileContents[leader], fileContents[(leader + 2) % SERVER_PORTS.size()]));
             }
 
             for (int i : differences.keySet()){
