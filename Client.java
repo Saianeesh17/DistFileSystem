45ad6f83cb85ac26e77aa9e9fb2241d403c82bdb
Saianeesh Vinmani <imsaianeesh17@gmail.com>
2024-04-08 19:51:01 -0600

    added derek's cleaned client code

--- Client.java
+++ Client.java
@@ -16,6 +16,7 @@ public class Client {
     private static DataInputStream dis;
     static String saveDirectory = "./client_received_files/";
 
+    //Starts the connection to the load balancer by initializing the socket and its input and output streams 
     public void startConnection(String ip, int port) {
 
         try {
@@ -26,7 +27,7 @@ public class Client {
             e.printStackTrace();
         }
     }
-
+    // sendFile method sends a file object over the data output stream created in startConnection
     public static void sendFile(String filepath) throws Exception {
         File file = new File(filepath);
         FileInputStream fileInputStream = new FileInputStream(file);
@@ -34,6 +35,7 @@ public class Client {
         long fileSize = file.length();
 
         try {
+            // write the request and file's name to the server
             dos.writeUTF("UPLOAD");
             dos.writeUTF(file.getName());
             // Send file size
@@ -49,47 +51,59 @@ public class Client {
         }
     }
 
+    // getFile reads a file over the data input stream (dis) and writes it locally
     private static void getFile(String filename) throws Exception {
+        // write the request and file's name to the server
         dos.writeUTF("GET");
         dos.writeUTF(filename);
         try{
             long fileSize = readWithTimeout(dis, 4000);
+            // Create a FileOutputStream to write the file to /client_received_files
             FileOutputStream fos = new FileOutputStream(saveDirectory + filename);
             byte[] buffer = new byte[4096];
         
             int bytesRead;
+            // write the file
             while (fileSize > 0 && (bytesRead = dis.read(buffer, 0, (int) Math.min(buffer.length, fileSize))) != -1) {
                 fos.write(buffer, 0, bytesRead);
                 fileSize -= bytesRead;
             }
             fos.close();
+            //alert user that file has been successfully loaded
             System.out.println("File successfully received!");
         }catch(TimeoutException e){
+            //alert user that file is not on any servers
             System.out.println("File does not exist");
         }
         
     }
+
+         // readWithTimeout allows us to read from an input stream, within an alotted amount of time
     public static long readWithTimeout(DataInputStream in, long timeoutMillis) throws IOException, TimeoutException {
+        // Record the starting time
         long startTime = System.currentTimeMillis();
         while (System.currentTimeMillis() - startTime < timeoutMillis) {
+            // Check if any data is available for reading
             if (in.available() > 0) {
             return in.readLong();
             }
             try {
-            Thread.sleep(10); // Adjust sleep time based on your needs
+            Thread.sleep(10); 
             } catch (InterruptedException e) {
-            // Handle interruption (optional)
             }
         }
-        // Timeout reached, throw an exception or handle as needed
+        // Timeout reached, throw an exception 
         throw new TimeoutException("No data received within timeout");
         }
 
+    // deleteFile allows us to remove files on the server
     public static void deleteFile(String filename) throws Exception {
+        // write the request and file's name to the server
         dos.writeUTF("DELETE");
         dos.writeUTF(filename);
     }
 
+    // simply terminates the connection between client and load balancer
     public static void stopConnection() {
 
         try {
@@ -105,11 +119,16 @@ public class Client {
 
         String[] serverAddresses = new String[] { "127.0.0.1", "127.0.0.1" };
         int[] portVals = new int[] { 2027, 2029 };
+        // Create the Client object and Scanner for user to input requets
         Client client = new Client();
+
         Scanner scanner = new Scanner(System.in);
+        //prompt user for requests
         System.out.println(
                 "Enter your new command from the list: \nWrite UPLOAD <File name> to upload a file to the server\n" +
                         "Write GET <File name> to get receive a file from the server \nWrite DELETE <File name> to delete a file from the server \nWrite QUIT to exit");
+        
+        // Read user requests and process commands
         String command = scanner.nextLine();
         
         while (!command.equals("QUIT")) {
@@ -119,6 +138,7 @@ public class Client {
                 case "UPLOAD":
                     boolean checkFile = false;
                     try{
+                        // Attempt to establish a socket connection to load balancer
                         clientSocket = new Socket(serverAddresses[addressVal], portVals[addressVal]);
                     }catch(ConnectException e){
                         System.out.println("Loadbalancer 1 failed");
@@ -129,6 +149,7 @@ public class Client {
                     
                     client.startConnection(serverAddresses[addressVal], portVals[addressVal]);
                     try {
+                        // Check to make sure the already file exists on the server. If it does, the upload is rejected. If it doesn't exist on local device to upload to server, alert user. Else, let user know file upload was successful.
                         File directory = new File(System.getProperty("user.dir"));
                         File[] files = directory.listFiles();
                         if (files != null) { 
@@ -154,7 +175,9 @@ public class Client {
                     
                 break;
                 case "DELETE":
+                    
                     try{
+                        //connect to load balancer
                         clientSocket = new Socket(serverAddresses[addressVal], portVals[addressVal]);
                     }catch(ConnectException e){
                         System.out.println("Loadbalancer 1 failed");
@@ -164,20 +187,21 @@ public class Client {
                     }
                     client.startConnection(serverAddresses[addressVal], portVals[addressVal]);
                     try {
-
+                        // Send delete command for the specified file
                         deleteFile(parameters[1]);
                         System.out.println("File successfully deleted!");
                     }catch(ArrayIndexOutOfBoundsException e){
                         System.out.println("No file entered");
                     }catch (Exception e) {
-                        // TODO Auto-generated catch block
                         e.printStackTrace();
                     }
                     stopConnection();
                     
                 break;
                 case "GET":
+                
                     try{
+                        //connect to load balancer
                         clientSocket = new Socket(serverAddresses[addressVal], portVals[addressVal]);
                     }catch(ConnectException e){
                         System.out.println("Loadbalancer 1 failed");
@@ -187,12 +211,12 @@ public class Client {
                     }
                     client.startConnection(serverAddresses[addressVal], portVals[addressVal]);
                     try {
+                        // Request the specified file from the server
                         getFile(parameters[1]);
                         
                     }catch(ArrayIndexOutOfBoundsException e){
                         System.out.println("No file entered");
                     }catch (Exception e) {
-                        // TODO Auto-generated catch block
                         e.printStackTrace();
                     }
                     stopConnection();
@@ -202,6 +226,7 @@ public class Client {
                 break;
 
             }
+            // Prompt to get next request from user
             System.out.println(
                     "Enter your new command from the list: \nWrite UPLOAD <File name> to upload a file to the server\n"
                             + "Write GET <File name> to get receive a file from the server \nWrite DELETE <File name> to delete a file from the server \nWrite QUIT to exit");
